75c833f0f1036332e7c08356af9365471e0c3894
Yurii Arkhypenko <99102508+SlonMoska@users.noreply.github.com>
2022-12-04 21:13:38 +0100

    new

---
+++ homework BF/Fourth/script.js
@@ -0,0 +1,86 @@
+// Task 1
+// let d = 7;
+// console.log (d)
+// console.log (d ** 2)
+// console.log (d ** 3)
+
+//Task 2
+// let num = Number(prompt("Number"));
+// let number = num % 2;
+// if(number == 0){
+//     console.log(num * 2);
+// }
+
+
+//Task 3 
+// let num = Number(prompt("Number"));
+// if(num > 0){
+//     console.log("plus");
+// }
+// else if(num < 0){
+//     console.log("minus")
+// }
+
+// else{
+//     console.log("zero");
+// }
+
+//Task 4
+// let a = Number(prompt('Введите первое число'));
+// var b = Number(prompt('Введите первое число'));
+// if (a > b) {
+//   console.log (a + b);
+// }
+// else if (a == b) {
+//   console.log ('good')
+// }
+// else {
+//   console.log (a * b)
+// }
+
+//Task 5
+// let num = Number(prompt("Go"))
+// if (num < 0){
+//   console.log(num ** 2)
+// }
+// else (
+//   console.log ('Error')
+// )
+
+//Task 6
+// let child = Number(prompt("children"))
+// let chair = Number(prompt("armchair"))
+// if (child <= chair) {
+//   console.log ('Good')
+// }
+// else {
+//   console.log ('no good')
+// }
+
+//Task 7
+// var  i;
+// alert('старт');
+// for (i = 1; i <= 9; i++) {
+//   console.log(i);
+// }
+// console.log('финиш');
+
+//Task 8
+// var i;
+// for (i = 5; i <= 99; i++) {
+// if (i % 5 == 0) {
+// console.log(i); 
+// }}
+
+//Task 9 
+// let a = Number(prompt('Введите число'));
+// var i;
+// if (a % 2 == 0) {
+// a = a - 1;
+// }
+// for (i = 1; i <= 7; i++) {
+// a = a + 2;
+// console.log(a);
+// }
+
+//Task 10
