b7d7cb8c88346c9e0767ea6ee6faafa7cc93c33b
Yurii Arkhypenko <99102508+SlonMoska@users.noreply.github.com>
2022-12-14 19:23:52 +0100

    new

---
+++ Front-End/lesson/script.js
@@ -0,0 +1,16 @@
+// let, const, var.  Разница учить
+
+// alert, prompt   Что это
+
+// const age =  prompt("Haw old are you?");
+// console.log(+age * 365);
+
+// const num = "5";
+// console.log(num, typeof num);
+
+// const newNum = +num;
+// console.log(newNum, typeof newNum)
+
+
+const number = prompt("Number");
+console.log(number**2);
