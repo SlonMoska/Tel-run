becb0debd98ebee7bbb41f09412fe69d50845788
slonmoska <arkhypenkoua@gmail.com>
2023-01-07 16:01:18 +0100

    new

--- Front-End/lesson/script.js
+++ Front-End/lesson/script.js
@@ -1,20 +1,19 @@
-// let, const, var.  Разница учить
+// let, const, var.              0Разница учить
+// const -  это постоянная 
+// var - это устаревшая
+// let - можем динамически изменить
 
-
-// alert, prompt   Что это
-
+// alert, prompt   Что это 
+// alert выводит сообщение.
+//  prompt выводит сообщение и ждёт, пока пользователь введёт текст, а затем возвращает введённое значение или null , если ввод отменён
 
 // const age =  prompt("Haw old are you?");
 // console.log(+age * 365);
 
-
 // const num = "5";
 // console.log(num, typeof num);
-
-
 // const newNum = +num;
 // console.log(newNum, typeof newNum)
-
 
 // const number = prompt("Number");
 // console.log(number**2);
@@ -27,6 +26,28 @@
 // console.log("b" + "a" + +"a" + "a");
 // baNaNa
 
+// const a = 0;
+// if (a > 0) {
+//   console.log("yes")
+// } else if (a < 0) {
+//   console.log("no");
+// } else {
+//   console.log("maybe");
+// }
+
+
+// const num = prompt("add a number:");
+// if(+num  === 0) {
+//   console.log("this is good")
+// } else if (+num % 2 !== 0) {
+//   console.log("this is not good")
+// } else  if (+num % 2 === 0) {
+//   console.log("very bad")
+// }
+
+
+// == нестрогое 
+// === строгое, сравнивает по типу ) 
 
 // const array = [1,7,5,4,3,2]
 // for(let i = 0; i < array.length; i++) {
@@ -35,9 +56,9 @@
 //   }
 // }
 
-const array = [15,8,4,2,4,7,12,35,45,58]
-for (let i = 1; i < array.length; i++) {
-  if (i % 3 === 0) {
-      console.log(array[i]);
-  }
-}
+// const array = [15,8,4,2,4,7,12,35,45,58]
+// for (let i = 1; i < array.length; i++) {
+//   if (i % 3 === 0) {
+//       console.log(array[i]);
+//   }
+// }
